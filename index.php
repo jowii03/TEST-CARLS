441def7d297ff708a04afc6f717d5a97628b1b69
itsmarkyy <79686026+itsmarkyy@users.noreply.github.com>
2021-05-12 01:53:02 +0800

    created header.php and footer.php

--- index.php
+++ index.php
@@ -4,71 +4,8 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <title>CarLS</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="theme-color" content="#03a6f3">
-
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/styles.css">
-    <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
-     <!-- Favicon -->
-    <link rel="shortcut icon" href="images/Carls-logo.png">
-</head>
-<body>
-    <header>        
-        <div class="main-menu title" style="">
-            <div class="container">
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="navbar-brand" href="index.php"><img class="brand-logo" src="images/Carls-logo.png" alt="logo"></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="animate-nav collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="navbar-item active">
-                                <a href="index.php" class="nav-link">Home</a>
-                            </li>
-                            <li class="navbar-item">
-                                <a href="modules.php" class="nav-link">Modules</a>
-                            </li>
-                            <?php
-                            if (isset($_SESSION['account_username'])) {  ?>
-                            <li class="navbar-item">
-                                <a href="assesment.php" class="nav-link">Assesment</a>
-                            </li><?php } else{ ?>
-                            <li class="navbar-item">
-                                <a href="login.php" class="nav-link">Assesment</a>
-                            </li> <?php } ?>
-                            <li class="navbar-item">
-                                <a href="license-info.php" class="nav-link">License Info</a>
-                            </li>
-                            <?php
-                            if (isset($_SESSION['account_username'])) { ?>
-                            <li class="navbar-item">
-                                <a href="profile.php" class="nav-link"><?php echo $_SESSION['account_username']; ?> 
-                                </a>
-                            </li>
-                             <li class="navbar-item">
-                                <a href="#" id="btnlogout" name="btnlogout" class="nav-link">Log Out</a>
-                            </li>
-                        <?php } else{ ?>
-                            <li class="navbar-item">
-                                <a href="login.php" class="nav-link">Sign in</a>
-                            </li>
-                        <?php } ?>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </div>
-    </header>
-    
+    <?php include 'header.php'; ?>
+    <body>
     <section class="slider" id="home">
         <div class="container">
             <div id="owl-demo" class="owl-carousel owl-theme">
@@ -292,50 +229,8 @@
             <img src="images/right-quote.png" alt="quote">
         </div>
     </section>
-    <footer id="contact">
-        <div class="container">
-           
-               
-                   
-                        <div class="col-md-12">
-                            <div class="address">
-                                <h4>List of LTO in Manila</h4>
-                                <h6>LTO Navotas</h6>
-                                <h6>LTO Mandaluyong</h6>
-                                <h6>LTO National Capital Region (NCR)</h6>
-                                <h6>LTO Malabon</h6>
-                                <h6>LTO Las Piñas</h6>
-                                <h6>LTO Makati</h6>
-                                <h6>LTO La Loma</h6>
-                                <h6>LTO Malabon</h6>
-
-                            </div>
-                        </div>
-                           
-                
-             
-         
-            
-        </div>
-        <div class="copy-right">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                        <h5>(C) 2020. CarLS UCC</h5>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="share align-middle">
-                            <span class="fb"><i class="fa fa-facebook-official"></i></span>
-                            <span class="instagram"><i class="fa fa-instagram"></i></span>
-                            <span class="twitter"><i class="fa fa-twitter"></i></span>
-                    
-                            <span class="google"><i class="fa fa-google-plus"></i></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    
+    <?php include 'footer.php'; ?>
 
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
